dc96aa4e9d807dd8deb89d821d7f0fc0aaa53132
Alexander Ybring <ybring@student.chalmers.se>
2021-01-24 13:51:07 +0100

    dates

--- js/countdowns/countdown_h.js
+++ js/countdowns/countdown_h.js
@@ -1,6 +1,6 @@
 
 // Set the date we're counting down to
-var countDownDate = new Date("November 16, 2019 22:30:00").getTime();
+var countDownDate = new Date("June 09, 2023 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
