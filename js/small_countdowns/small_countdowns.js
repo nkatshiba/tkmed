f24fa30313198d7a5f87f5f219164feaca612989
Alexander Ybring <ybring@student.chalmers.se>
2021-01-25 10:05:53 +0100

    checklista klar

--- js/small_countdowns/small_countdowns.js
+++ js/small_countdowns/small_countdowns.js
@@ -1,5 +1,5 @@
 n =  new Date();
 m = n.getMonth() + 1;
 d = n.getDate();
-document.getElementById("dagens1").innerHTML = d + "/" + m;
+document.getElementById("dagens1").innerHTML = "Idag är den " + d + "/" + m;
 document.getElementById("dagenstop").innerHTML = m + "/" + d;
