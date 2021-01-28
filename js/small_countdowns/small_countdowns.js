f0d111af74d886a254c979dc104b92ec5cd2aa92
Alexander Ybring <ybring@student.chalmers.se>
2021-01-28 16:19:03 +0100

    index snyggare

--- js/small_countdowns/small_countdowns.js
+++ js/small_countdowns/small_countdowns.js
@@ -2,4 +2,4 @@ n =  new Date();
 m = n.getMonth() + 1;
 d = n.getDate();
 document.getElementById("dagens1").innerHTML = "Idag är den " + d + "/" + m;
-document.getElementById("dagenstop").innerHTML = m + "/" + d;
+document.getElementById("dagens2").innerHTML = "Idag är den " + d + "/" + m;
